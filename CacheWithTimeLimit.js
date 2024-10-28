798de1690cb863bb7a75c2c5c7beb04c70611723
PQMIT <PQM2112000@gmail.com>
2024-10-28 17:05:43 +0700

    update

--- CacheWithTimeLimit.js
+++ CacheWithTimeLimit.js
@@ -1,4 +1,39 @@
-var TimeLimitedCache = function () {};
-TimeLimitedCache.prototype.set = function (key, value, duration) {};
-TimeLimitedCache.prototype.get = function (key) {};
-TimeLimitedCache.prototype.count = function () {};
+var TimeLimitedCache = function () {
+    this.cache = new Map();
+};
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+    if (this.cache.has(key)) {
+        this.cache.set(key, { value: value, duration: Date.now() + duration });
+        return true;
+    }
+    this.cache.set(key, { value: value, duration: Date.now() + duration });    
+    return false;
+};
+TimeLimitedCache.prototype.get = function (key) {
+    if (this.cache.has(key)) {
+        let value = this.cache.get(key);
+        if (value.duration >= Date.now()) {
+            return value.value;
+        } else {
+            this.cache.delete(key);
+            return -1;
+        }
+    } else {
+        return -1;
+    }
+};
+TimeLimitedCache.prototype.count = function () {
+    let count =0;
+    for (let [key, value] of this.cache) {
+        if (value.duration >= Date.now()) {
+            count++;
+        }
+    }
+    return count;
+};
+
+
+const timeLimitedCache = new TimeLimitedCache()
+console.log(timeLimitedCache.set(1, 42, 1000)); // false
+console.log(timeLimitedCache.get(1)); // 42
+console.log(timeLimitedCache.count()); // 1
---
+++ Debounce.js
@@ -0,0 +1,14 @@
+var debounce = function(fn, t) {
+    let timer;
+    return function(...args) {
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+            fn(...args);
+        }, t);
+    }
+};
+
+const log = debounce(console.log, 50);
+log('Hello'); // cancelled
+log('Hello'); // cancelled
+log('Hello'); // Logged at t=100ms
